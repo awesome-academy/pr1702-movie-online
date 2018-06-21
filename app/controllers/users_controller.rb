ee32980c565c5a4e09b701840061d92c58a2ffba
Nguyen Ba The <nguyenbathe98@gmail.com>
2018-06-21 22:40:09 +0700

    I18n Fix 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def index
-    @users = User.paginate(page: params[:page], per_page: 10)
+    @users = User.paginate(page: params[:page], per_page: Settings.user.index)
     @relationship = Relationship.new
   end
 
   def show
-  	@relationship = Relationship.new
+    @relationship = Relationship.new
     @user = User.find_by id: params[:id]
     redirect_to users_path unless @user
   end
